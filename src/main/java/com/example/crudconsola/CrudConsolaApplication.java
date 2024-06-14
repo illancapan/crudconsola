0e03055e5ae392580bb875b0f67b958466f93ed2
illancapan <igorllancapan@gmail.com>
2024-06-14 10:42:44 -0400

    elimino public, de EspecialidadRepository

--- src/main/java/com/example/crudconsola/CrudConsolaApplication.java
+++ src/main/java/com/example/crudconsola/CrudConsolaApplication.java
@@ -1,14 +1,20 @@
 package com.example.crudconsola;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudConsolaApplication {
+public class CrudConsolaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(CrudConsolaApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo");
+	}
 }
